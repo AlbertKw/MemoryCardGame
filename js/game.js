e9f1d56d16a75dd5c21fb316a81d0668d135583c
Albert <albert.kwiecinski130@gmail.com>
2020-09-21 00:10:47 +0200

    Improve performace
    The performance is improved by limiting of creation new eventHandlers.
    Before init() method in game.js was called every new game.

--- js/game.js
+++ js/game.js
@@ -13,20 +13,28 @@ const Gameboard = {
     cards: [],
   },
 
-  init() {
-    this.domElements.cardsContainer = document.querySelector(".cards-container");
-    this.domElements.startButton = document.querySelector(".start-game");
-    console.log(this.domElements.startButton);
-    this.domElements.cardsNumberSlider = document.getElementById("numberOfCardsSlider");
-    this.domElements.startButton.addEventListener("click", () => {
-      this.properties.cards.forEach(card => {
-        card.setAttribute("src", `img/card-back.jpg`);
-        card.dataset.faceup = false;
-        card.style.pointerEvents = "auto";
-      });
-      this.domElements.startButton.style.visibility = "hidden";
-    })
+  init: (function() {
+    let executed = false;
+    return () => {
+      if(!executed) {
+        this.domElements.cardsContainer = document.querySelector(".cards-container");
+        this.domElements.startButton = document.querySelector(".start-game");
+        this.domElements.cardsNumberSlider = document.getElementById("numberOfCardsSlider");
+        this.domElements.startButton.addEventListener("click", () => {
+          this.properties.cards.forEach(card => {
+            card.setAttribute("src", `img/card-back.jpg`);
+            card.dataset.faceup = false;
+            card.style.pointerEvents = "auto";
+          });
+          this.domElements.startButton.style.visibility = "hidden";
+        })
+        executed = true;
+      }
+      this.newGame();
+    }
+  }),
 
+  newGame() {
     this.properties.numberOfCards = Math.floor(this.domElements.cardsNumberSlider.value / 2);
     this.properties.cardsNames = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'].slice(0, this.properties.numberOfCards);      // Temporary, then cards are taken from img folder, then from database
     this.createCards();
@@ -35,7 +43,6 @@ const Gameboard = {
     this.properties.cards.forEach(card => {
       this.domElements.cardsContainer.appendChild(card);
     });
-    this.domElements.cardsContainer.appendChild(this.properties.cards[1]);
   },
 
   createCards() {
@@ -58,7 +65,7 @@ const Gameboard = {
         this.properties.faceUpCards.push(card);
         if(this.properties.faceUpCards.length == 2) {
           this.properties.cards.forEach(card => {
-            card.style.pointerEvents = "none"
+            card.style.pointerEvents = "none";
           });
           await sleep(500);
           this.checkIfCardsAreTheSame();
@@ -91,8 +98,6 @@ const Gameboard = {
         card.style.pointerEvents = "none";
       });
       this.properties.points++;
-      if(this.properties.faceUpCards.length === this.properties.numberOfCards * 2)
-        this.endGame();
     } else {
       faceUpCards.forEach(card => {
         card.setAttribute("src", `img/card-back.jpg`);
@@ -105,6 +110,7 @@ const Gameboard = {
 
   endGame() {
     this.domElements.cardsContainer.innerHTML = "";
+    this.domElements.startButton.style.visibility = "visible";
   },
 }
 
@@ -113,5 +119,5 @@ const sleep = ms => {
 }
 
 document.querySelector(".new-game-btn").addEventListener("click", () => {
-  Gameboard.init();
+  Gameboard.init()();
 });
